a3fcbf7fa0632acfbf99c7131d2a1198084ec8d4
Gabriel Walters <gwalters@lhschools.org>
2018-08-29 14:17:23 -0400

    Set up time of day to overload constructor, improving validity

--- src/ClassOverloading.java
+++ src/ClassOverloading.java
@@ -1,2 +1,13 @@
+import java.util.Scanner;
+
 public class ClassOverloading {
+
+
+    public static void main(String[] args){
+        Scanner keyboard = new Scanner(System.in);
+        System.out.println("Enter a time seperate the hour and minutes with a space");
+        TimeOfDay now = new TimeOfDay(keyboard.nextInt(),keyboard.nextInt());
+
+        System.out.println(now.getmHour() +": "+ now.getmMinutes());
+    }
 }
